a37124d25760b93df81e9a477b02650919b2d582
Dhruvpal <palsahab427@gmail.com>
2026-06-07 10:43:13 +0530

    Removed Unwanted code

--- server.js
+++ server.js
@@ -3,25 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
-const allowedOrigins = [
-  "https://admin.soulfulindiatour.com",
-  "https://www.soulfulindiatour.com"
-];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, etc.)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: ["https://admin.soulfulindiatour.com", "https://www.soulfulindiatour.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
-// Handle preflight requests
-app.options("*", cors());
 
 require('./src/app');
